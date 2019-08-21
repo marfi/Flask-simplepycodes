c58f8b2e52dfe7d22b3d55605afa3ea8b4af7791
Xerex <frankdezz@gmail.com>
2019-08-21 14:45:03 +0200

    Update auto_mail.py

--- auto_mail.py
+++ auto_mail.py
@@ -2,8 +2,8 @@ import smtplib
 
 def send_mail(to, msg):
     server = smtplib.SMTP_SSL('smtp.gmail.com',465)
-    server.login("simplepycodetest@gmail.com", "Test5050?")
+    server.login("your email", "your password")
     try:
-        server.sendmail("simplepycodetest@gmail.com",to, msg)
+        server.sendmail("your email",to, msg)
     except Exception:
         send_mail()
